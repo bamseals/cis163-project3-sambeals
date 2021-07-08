68f75be7018334ebbd5f964e34fe76a093762341
Sam Beals <bealssa@mail.gvsu.edu>
2021-07-07 20:10:47 -0400

    added junit

---
+++ src/Project3Test.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class Project3Test{
+    @Test
+    public void test123(){
+
+    }
+}
